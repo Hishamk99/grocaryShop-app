4f1e929ec813b50b9b3d3ad2264220da0ada46e2
hishamkame <hishamkamel370@gmail.com>
2024-04-21 18:26:00 +0200

    create category model

---
+++ lib/models/category_model.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CategoryModel {
+  final String tittle;
+  final Color color;
+
+  CategoryModel({
+    required this.tittle,
+    required this.color,
+  });
+}
