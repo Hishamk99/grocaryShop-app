8d40550f6382a8ee2b40e6a57e23c1f6c6dd6bcd
hishamkame <hishamkamel370@gmail.com>
2024-04-21 18:39:50 +0200

    create home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/screens/home_page.dart';
 
 void main() {
   runApp(const GrocaryShop());
@@ -11,7 +12,7 @@ class GrocaryShop extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: HomePage(),
     );
   }
 }
