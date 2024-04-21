8cba5e8aee276f98f563ad966b6a004ab9cf4d99
hishamkame <hishamkamel370@gmail.com>
2024-04-21 18:28:13 +0200

    create grocery model

---
+++ lib/models/grocery_model.dart
@@ -0,0 +1,15 @@
+import 'package:shop_app/models/category_model.dart';
+
+class GroceryModel {
+  final String id;
+  final String name;
+  final int quantity;
+  final CategoryModel category;
+
+  GroceryModel({
+    required this.id,
+    required this.name,
+    required this.quantity,
+    required this.category,
+  });
+}
